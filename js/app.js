57fe149369eed404f381579a5ca5f07e3bc5d141
jacobjelen <jacobjelen@gmail.com>
2022-12-10 21:29:21 +0100

    ADDED to app.js: - logic to handle the incoming value/byte in handleNotification() - update_touchpad() to display show pressure on the insole

--- js/app.js
+++ js/app.js
@@ -3,21 +3,20 @@
 const bleNusServiceUUID  = '713d0000-503e-4c75-ba94-3148f18d941e'; 
 const bleNusCharTXUUID   = '713d0002-503e-4c75-ba94-3148f18d941e'; 
 
-
-// // ORIGINAL
-// const bleNusServiceUUID  = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';  
-// const bleNusCharRXUUID   = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'; 
-// const bleNusCharTXUUID   = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
-
 const MTU = 20;
 
 var bleDevice;
 var bleServer;
 var nusService;
-// var rxCharacteristic;
 var txCharacteristic;
 
 var connected = false;
+
+const touchpad = { 
+    x: 0, 
+    y: 0, 
+    z: 0 
+}
 
 function connectionToggle() {
     if (connected) {
@@ -66,14 +65,7 @@ function connect() {
         nusService = service;
         console.log('Found NUS service: ' + service.uuid);
     })
-    // .then(() => {
-    //     console.log('Locate RX characteristic');
-    //     return nusService.getCharacteristic(bleNusCharRXUUID);
-    // })
-    // .then(characteristic => {
-    //     rxCharacteristic = characteristic;
-    //     console.log('Found RX characteristic');
-    // })
+
     .then(() => {
         console.log('Locate TX characteristic');
         return nusService.getCharacteristic(bleNusCharTXUUID);
@@ -127,17 +119,84 @@ function onDisconnected() {
     setConnButtonState(false);
 }
 
+/* Data is comming in as 5 consequtive bytes
+[0] 170 (AA)    = First byte Flag
+[1] 0-1023      = xLocation
+[2] 187 (BB)    = Second Flag
+[3] 0-1023      = yLocation
+[4] 0-50        = Activation
+ */
+
+
+let head = 0;
+
 function handleNotifications(event) {
     console.log('notification');
     let value = event.target.value;
-    // Convert raw data bytes to character values and use these to 
-    // construct a string.
+    // Convert raw data bytes to character values and use these to construct a string.
     let str = "";
+
     for (let i = 0; i < value.byteLength; i++) {
-        str += value.getUint8(i) //String.fromCharCode(value.getUint8(i));
+        // str += String.fromCharCode(value.getUint8(i));
+        
+        str += value.getUint8(i) // this will be a decimal number representing the incoming byte
+
+        // Head will keep track of which byte out of the expected 5 we are reading
+        if (str == '170') {
+            head = 0
+        } else {
+            head += 1
+        }
+
+        // Update the touchpad object
+        switch (head) {
+            case 1:
+              touchpad.x = str;
+              break;
+            case 3:
+                touchpad.y = str;
+              break;
+            case 4:
+                touchpad.z = str;
+              break;
+            // default:
+            //   console.log('x is something else');
+          }
+        
+        // update touchpad on the screen
+        update_touchpad()
     }
     window.term_.io.print(str);
 }
+
+function update_touchpad() {
+    const touchpad_threshold = 450
+  
+    const canvas = document.getElementById('tp_canvas')
+    const press = document.getElementById('press')
+  
+    const posx = touchpad.x / 4095 * canvas.clientWidth
+    const posy = touchpad.y / 4095 * canvas.clientHeight
+    const sizez = touchpad.z / 4095 * canvas.clientHeight / 2 // size of pressure cirle scales with the canvas/window
+    // console.log(`x: ${posx}  y: ${posy}  z: ${sizez}`)
+  
+    // press is a div inside the tp_canvas div representing position and force of pressure on the physical sensor
+    press.style.top = posy - (sizez / 2) // offset by a half of the size => center in the middle of press
+    press.style.left = posx - (sizez / 2)
+  
+    if (z > touchpad_threshold) {
+      // press.style.display = 'block'
+      press.style.width = sizez
+      press.style.height = sizez
+      press.style.opacity = z
+    } else {
+      // press.style.display = 'none'
+      press.style.width = 0
+      press.style.height = 0
+      press.style.opacity = 0
+    }
+  
+  }
 
 function nusSendString(s) {
     if(bleDevice && bleDevice.gatt.connected) {
